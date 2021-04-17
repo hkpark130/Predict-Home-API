acd1197ff38f20e0fd00d22b671f50c5097b3fbf
root <hkpark130@naver.com>
2021-04-17 21:09:29 +0000

    first init

--- data_handler/house_handler.py
+++ data_handler/house_handler.py
@@ -20,7 +20,13 @@ class HouseHandler(tornado.web.RequestHandler):
             self.write(message)
         try:
             # TODO:파라미터 넘겨서 집 값 예측
-            address = data['address']
+            address = data['address']               # 주소
+            area = data['area']                     # 면적
+            dis_to_station = data['dis_to_station'] # 역까지 거리
+            year_of_cons = data['year_of_cons']     # 건축년도
+            floors = data['floors']                 # 층수
+            separ_toilet = data['separ_toilet']     # 욕실 화장실 분리
+
             json_data = json.dumps(address)
             self.write(json_data)
         except:
